aa27e0185154d6a78f6883ce0d88b1af9502aa2e
Dima Denisenko <dendima80@gmail.com>
2025-01-24 17:22:32 +0200

    JIRA-RTY-956, ver-1.55

--- src/main/kotlin/com/eigenmethod/rentality/utilites/OtherUtils.kt
+++ src/main/kotlin/com/eigenmethod/rentality/utilites/OtherUtils.kt
@@ -23,8 +23,40 @@ inline fun <reified T> T?.orIfNull(input: () -> T): T {
 }
 
 // Проверка корректности введённой даты Wagmi
+//fun isValidShuttleDate(dateTime: String): Boolean {
+//    val validDates = setOf("2025-01-23", "2025-01-24")
+//    val date = dateTime.split("T").firstOrNull() // Получаем только дату до T (2025-01-23T10:30)
+//    return date in validDates
+//}
 fun isValidShuttleDate(dateTime: String): Boolean {
     val validDates = setOf("2025-01-23", "2025-01-24")
-    val date = dateTime.split("T").firstOrNull() // Получаем только дату до T (2025-01-23T10:30)
-    return date in validDates
+    val dateTimeParts = dateTime.split("T") // Разделяем дату и время
+    val date = dateTimeParts.getOrNull(0) // Получаем дату
+    val time = dateTimeParts.getOrNull(1) // Получаем время
+    console.log("ddiLog dateTimeParts=$dateTimeParts")
+    console.log("ddiLog date=$date")
+    console.log("ddiLog time=$time")
+
+    if (date !in validDates) return false // Проверяем дату
+
+    if (time != null) {
+        val parsedTime = time.split(":") // Парсим время из строки
+        val parsedHours = parsedTime[0].toInt()
+        val parsedMinutes = parsedTime[1].toInt()
+
+        val cutoffHours = 19 // Граничное время
+        val cutoffMinutes = 30 // Граничное время
+        console.log("ddiLog parsedHours=$parsedHours")
+        console.log("ddiLog parsedMinutes=$parsedMinutes")
+
+        // Сравниваем часы и минуты вручную
+        if (parsedHours > cutoffHours ||
+                (parsedHours == cutoffHours && parsedMinutes > cutoffMinutes)) {
+            return false
+        }
+    } else {
+        return false
+    }
+
+    return true
 }
